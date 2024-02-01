5333d6d03113bb0c27837c74b6612a97c2fbf87b
KangJiUng <107825793+KangJiUng@users.noreply.github.com>
2024-02-01 20:37:30 +0900

    Modify BOJ_11725.cpp

--- Week4/BOJ_11725.cpp
+++ Week4/BOJ_11725.cpp
@@ -1,48 +1,39 @@
-//백준 1158번 요세푸스 문제
+//백준 11725번 트리의 부모 찾기
 
 #include <iostream>
-#include <list>
+#include <vector>
+#define MAX 100001
 using namespace std;
 
-int main(void) {
-    int N, K;
-    list<int> L;
+int node[MAX]; //각 노드에 대한 부모 노드를 저장하는 배열
+bool visited[MAX] = { false }; //각 노드를 방문했음을 표시하는 배열
+vector<int> v[MAX]; //무방향 트리(노드 연결만)를 저장하는 벡터
 
-    cin >> N >> K;
-
-    if (N == 1) { // N이 1이면 그냥 <1>을 출력하고 끝냄
-        cout << "<1>";
-        return 0;
+void dfs(int k) {
+    visited[k] = true; //방문했음을 표시
+    for (int i = 0; i < v[k].size(); i++) {
+        int parent = v[k][i]; 
+        if (!visited[parent]) { //방문하지 않은 노드라면
+            node[parent] = k; //현재 노드에 연결된 노드를 부모 노드로 우선 설정
+            dfs(parent); //재귀호출
+        }
     }
-    else {
-        for (int i = 1; i <= N; i++) { // 1~N 까지의 숫자 리스트에 추가
-            L.push_back(i);
-        }
+}
 
-        list<int>::iterator iter = L.begin();
+int main() {
+    int N;
+    cin >> N;
 
-        cout << "<";
+    for (int i = 0; i < N-1; i++) { //둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어짐
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
 
-        while (L.size()) {
-            for (int i = 1; i < K; i++) { // 1번째 사람부터 K-1번째 사람까지
-                if (++iter == L.end()) { // 다음 반복자가 리스트의 끝이면
-                    iter = L.begin(); // 반복자를 다시 처음으로
-                    continue;
-                }
-            }
+    dfs(1);
 
-            if (L.size() == 1) {
-                cout << *iter;  // 반복자는 * 연산자를 사용하여 컨테이너 내부의 원소를 가리키고 접근할 수 있어야함
-            }
-            else {
-                cout << *iter << ", ";
-            }
-
-            iter = L.erase(iter);
-
-            iter = (iter == L.end()) ? L.begin() : iter; // 삭제한 원소 자리가 이미 end인 경우 begin으로 보냄
-        }
-
-        cout << ">";
+    for (int i = 2; i <= N; i++) {
+        cout << node[i] << "\n";
     }
 }
